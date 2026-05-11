a4205d67f8484e143244ef894d7a30c6010c050d
FlagQuantum <liuwei.chem.phys@gmail.com>
2026-05-11 21:36:16 +0800

    fix matplotlib dependency

--- flagquantum/drawer/__init__.py
+++ flagquantum/drawer/__init__.py
@@ -2,9 +2,18 @@
 FlagQuantum 电路绘图模块
 """
 
-from .mpl_drawer import draw_mpl
 from .style import available_styles, use_style
-from .text_drawer import draw_text
+from .text_drawer import TextDrawer, draw_text
+
+# mpl 是可选依赖，未安装 matplotlib 时不影响 text 模式
+try:
+    from .mpl_drawer import MPLDrawer, draw_mpl
+
+    _has_mpl = True
+except ImportError:
+    _has_mpl = False
+    draw_mpl = None  # type: ignore
+    MPLDrawer = None  # type: ignore
 
 
 def draw(qdev, format="text", **kwargs):
@@ -18,11 +27,20 @@ def draw(qdev, format="text", **kwargs):
 
     Returns:
         text模式返回字符串，mpl模式返回 (fig, ax)
+    Raises:
+        ImportError: 当 format="mpl" 但 matplotlib 未安装时
     """
     if format == "mpl":
+        if not _has_mpl:
+            raise ImportError(
+                "matplotlib is required for `format='mpl'`. "
+                "Install it with: pip install flagquantum[viz]"
+            )
         return draw_mpl(qdev, **kwargs)
-    else:
-        return draw_text(qdev, **kwargs)
+    return draw_text(qdev, **kwargs)
 
 
-__all__ = ["draw_text", "draw_mpl", "available_styles", "use_style", "draw"]
+__all__ = ["TextDrawer", "draw_text", "available_styles", "use_style", "draw"]
+
+if _has_mpl:
+    __all__ += ["MPLDrawer", "draw_mpl"]
